d54be83a77b3657332f15209b969443107c001a6
Daniil <rychenkov16@yandex.ru>
2025-02-15 21:10:05 +0300

    add function 1

--- LifeSpot/Style/JS/index.js
+++ LifeSpot/Style/JS/index.js
@@ -25,3 +25,15 @@ for (let result of session) {
     console.log(result)
 }
 
+function showTranslation() {
+    let inputString=document.getElementsByTagName('input')[0].value.toLowerCase();
+    let elements=document.getElementsByClassName('video-container');
+    for (let i = 0; i <= elements.length; i++){
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
